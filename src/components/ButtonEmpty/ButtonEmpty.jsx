b159d3c3dee4f567e2dffa5a285d9813fd892e9a
Евгений Степанишин <stepanishin92@mail.ru>
2022-08-14 09:10:16 +0200

    setBtns

---
+++ src/components/ButtonEmpty/ButtonEmpty.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ButtonEmpty.css'
+
+const ButtonEmpty = () => {  
+    
+    return (
+            <section style={{width: '30vw'}}>
+                <button className="figur-button-empty">
+                <div></div>
+                <span></span>
+                </button>
+            </section>
+    );
+};
+
+export default ButtonEmpty;
---
+++ src/components/ButtonFill/ButtonFill.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ButtonFill.css'
+
+const ButtonFill = () => {  
+    
+    return (
+        <section style={{width:'30vw'}}>
+            <button class="figur-button-filled">
+            <div></div>
+            <span></span>
+            </button>
+      </section>
+    );
+};
+
+export default ButtonFill;
